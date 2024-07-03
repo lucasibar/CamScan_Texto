9292a5b6143d3e6da7cc97222a6213c6c9033cf8
lucasibar <lucas.ibar.soy.henry@gmail.com>
2024-07-02 21:00:55 -0300

    base del proyecto terminado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-// src/App.js
 import React, { useEffect, useState } from 'react';
 import CameraComponent from './CameraComponent';
 import './App.css';
+import Tesseract from 'tesseract.js';
 
 const App = () => {
   const [photo, setPhoto] = useState(null);
+  const [recognizedText, setRecognizedText] = useState('');
 
-  useEffect(()=>{
-    console.log(photo)
-  },[photo])
+  useEffect(() => {
+    if (photo) {
+      recognizeText(photo);
+    }
+  }, [photo]);
 
   const handleCapture = (imageSrc) => {
     setPhoto(imageSrc);
+  };
+
+  const recognizeText = async (imageSrc) => {
+    try {
+      const { data: { text } } = await Tesseract.recognize(
+        imageSrc,
+        'eng',
+        { logger: m => console.log(m) }
+      );
+      console.log(text)
+      setRecognizedText(text);
+    } catch (error) {
+      console.error('Error en OCR:', error);
+    }
   };
 
   return (
@@ -23,6 +40,12 @@ const App = () => {
           <div>
             <h2>Foto Capturada:</h2>
             <img src={photo} alt="Captured" style={{ maxWidth: '100%' }} />
+            {recognizedText && (
+              <div>
+                <h3>Texto Reconocido:</h3>
+                <p>{recognizedText}</p>
+              </div>
+            )}
           </div>
         )}
       </header>
